254c19cae08b1ab8cc257e7229de04fd54dadfd1
Chule Cabral <chulesantos@outlook.com>
2020-10-23 16:18:28 -0300

    CheckPoint Initial

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+Route::get('/series', 'SeriesController@index')
+    ->name('listar_series');
+
+Route::get('/series/criar', 'SeriesController@create');
+
+Route::post('/series/criar', 'SeriesController@store')
+    ->name('criar_serie');
+
+Route::delete('/series/{id}', 'SeriesController@destroy')
+    ->name('excluir_serie');
