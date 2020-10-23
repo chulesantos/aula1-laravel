8d4c75868b095b7a7b97ca5a6a57fb4f3a68bd0f
Chule Cabral <chulesantos@outlook.com>
2020-10-23 17:01:15 -0300

    Término curso

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SeriesFormRequest;
 use App\Serie;
 use Illuminate\Http\Request;
 
@@ -26,15 +27,8 @@ class SeriesController extends Controller
 
     }
 
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
-
-        $request->validate(
-            [
-                'nome' => 'required|min:3'
-
-            ]
-        );
 
         $nome = $request->nome;
 
